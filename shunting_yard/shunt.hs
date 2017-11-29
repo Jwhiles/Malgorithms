63fe97a6cc7ce39cd64b944404041e3cd134830a
John Whiles <john.whiles@justgiving.com>
2017-11-29 15:57:04 +0000

    refactor shunter

--- shunting_yard/shunt.hs
+++ shunting_yard/shunt.hs
@@ -3,9 +3,21 @@ import           Data.List   (intersperse)
 import           Data.Maybe  (fromMaybe)
 import           Data.Monoid
 
+
+main :: IO ()
+main = print $ run "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"
+
+run input = mconcat . intersperse " " $ shunt (words input) [] []
+
 type Input = [String]
 type Stack = [String]
 type Result = [String]
+
+shunt :: Input -> Stack -> Result -> Result
+shunt (x:xs) (y:ys) r = handleInputAndStack (x:xs) (y:ys) r
+shunt (x:xs) _ r      = handleInput (x:xs) r
+shunt _ (y:ys) r      = handleStack (y:ys) r
+shunt _ _ r           = r
 
 operatorMap :: [(String, Int)]
 operatorMap = [("(", 10), ("^", 4), ("*", 3), ("/", 3), ("+", 2), ("-", 2)]
@@ -23,8 +35,8 @@ handleRightParen input (first:rest) result =
         _   -> handleRightParen input rest (result <> [first])
 handleRightParen input _ result = shunt input [] result
 
-shunt :: Input -> Stack -> Result -> Result
-shunt (x:xs) (y:ys) r 
+handleInputAndStack :: Input -> Stack -> Result -> Result
+handleInputAndStack (x:xs) (y:ys) r
     | isNumber (head x) = shunt xs (y:ys) (r <> [x])
     | x == ")" = handleRightParen xs (y:ys) r
     | y == "(" = shunt xs (x:y:ys) r
@@ -32,13 +44,13 @@ shunt (x:xs) (y:ys) r
     | comparePrecedence x y == EQ && x == "^" = shunt xs (x:y:ys) r
     | comparePrecedence x y == EQ = shunt (x:xs) ys (r <> [y])
     | otherwise = shunt (x:xs) ys (r <> [y])
-shunt (x:xs) _ r
+
+handleInput :: Input -> Result -> Result
+handleInput (x:xs) r
     | isNumber (head x) = shunt xs [] (r <> [x])
     | otherwise = shunt xs [x] r
-shunt _ (y:ys) r = shunt [] ys (r <> [y])
-shunt _ _ r = r
 
-run input = mconcat . intersperse " " $ shunt (words input) [] []
+handleStack :: Stack -> Result -> Result
+handleStack (x:xs) r = shunt [] xs (r <> [x])
 
-main :: IO ()
-main = print $ run "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"
+
